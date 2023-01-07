cd1c93cb37a2c3da012c1c8ea64de801685b9e72
ctlaranjeiro <ctlaranjeiro@gmail.com>
2023-01-07 02:47:04 +0000

    chore: removes unnecessary styling and adds new component for next Head

--- components/NextHead/index.tsx
+++ components/NextHead/index.tsx
@@ -1,3 +1,17 @@
-const NextHead = () => <div>This is a next head</div>
+import Head from "next/head"
+
+type NextHeadT = {
+  title: string
+  metaDescription: string
+}
+
+const NextHead = ({ title='NextJS app', metaDescription='NextJS app'}: NextHeadT) => (
+	<Head>
+		<title>Chat | {title}</title>
+		<meta name="description" content={`Chat | ${metaDescription}`} />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="/favicon.ico" />
+	</Head>
+)
 
 export default NextHead
